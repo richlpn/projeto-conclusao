5c5be56ad4784e87bcc969f05a1ea450ae4a44ba
richard.nunes <richardl1010@outlook.com>
2024-10-12 11:45:38 -0700

    ADD: Tool Answer model

---
+++ src/models/call_tools_model.py
@@ -0,0 +1,11 @@
+from typing import Generic, TypeVar
+
+from pydantic import BaseModel, Field
+
+T = TypeVar("T")
+
+
+class ToolCallAnswer(Generic[T], BaseModel):
+    tool_name: str = Field(..., description="The name of the tool that was called.")
+    input: str = Field(description="The input string")
+    output: T = Field(description="The llm response")
