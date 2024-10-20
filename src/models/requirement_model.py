8de5bd3ce69de6509bf5d2a7fe412f59b1747f1d
richard.nunes <richardl1010@outlook.com>
2024-10-20 10:34:55 -0700

    ADD: Requirent & Task models

---
+++ src/models/requirement_model.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+
+
+class Task(BaseModel):
+    description: str = Field(..., description="The description of the task.")
+    parameters: list[str] = Field(
+        default_factory=list,
+        description="A list of parameters to be used. Or considered when executing the task.",
+    )
+
+
+class Requirement(BaseModel):
+    title: str = Field(description="Requirement title")
+    tasks: list[Task] = Field(default_factory=list)
