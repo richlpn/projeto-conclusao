a97fc30cd7bbffb066685e82b02cab058b384d95
richard.nunes <richardl1010@outlook.com>
2024-10-20 10:28:34 -0700

    REMOVE: Old python modules from before merge

--- src/utils/get_model.py
+++
@@ -1,7 +0,0 @@
-from langchain_anthropic import ChatAnthropic
-from langchain_core.language_models import BaseChatModel
-from dotenv import load_dotenv
-
-load_dotenv('/home/rich/Code/01_Ifes/03_TCC/.env')
-
-MODEL: BaseChatModel = ChatAnthropic(model="claude-3-5-sonnet-20240620", temperature=0) # type: ignore
--- src/utils/load_env.py
+++
@@ -1,4 +0,0 @@
-from dotenv import load_dotenv
-
-
-load_dotenv('/home/rich/Code/01_Ifes/03_TCC/.env')
