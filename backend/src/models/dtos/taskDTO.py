ef051bb6854ae7c87f8abdedf8400664292161ab
richard.nunes <richardl1010@outlook.com>
2024-11-24 10:21:45 -0800

    ADD: TaskDTO

---
+++ backend/src/models/dtos/taskDTO.py
@@ -0,0 +1,13 @@
+
+
+from src.models.task import Task
+from src.utils.base_dto import BaseDTO
+
+
+class TaskDTO(BaseDTO[Task]):
+
+    title: str
+    description: str
+    singature_function: str
+
+    
