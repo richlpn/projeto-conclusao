3437534e6e18c4911ddcbe1a09180550255e32df
richard.nunes <richardl1010@outlook.com>
2024-11-24 11:17:38 -0800

    REFACTOR: Task ID default factory

--- backend/src/schema/task_schema.py
+++ backend/src/schema/task_schema.py
@@ -1,10 +1,14 @@
+from uuid import uuid4
 from pydantic import Field
 from sqlalchemy import UUID
+from src.models.task import Task
 from src.utils.base_schema import BaseSchema
 
 
-class TaskSchema(BaseSchema):
-    id: UUID = Field(..., alias="id", description="Unique identifier for the task.")
+class TaskSchema(BaseSchema[Task]):
+    id: UUID = Field(
+        default_factory=uuid4, alias="id", description="Unique identifier for the task."
+    )
     title: str = Field(description="A short descriptive title about the task")
     description: str = Field(
         description="A Detailed explanation of what must be executed on this task, containing inputs, ouputs and previous dependecies."
@@ -15,3 +19,10 @@ class TaskSchema(BaseSchema):
 
     def __str__(self) -> str:
         return f"Title: {self.title}\nDescription: {self.description}\n"
+
+    @classmethod
+    def from_model(cls, model: Task):
+        return cls(**model.__dict__)
+
+    def to_model(self) -> Task:
+        return Task(**self.model_dump())
