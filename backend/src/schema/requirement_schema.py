f603f4ce1311f2668218c733e1f70456c86fe5af
richard.nunes <richardl1010@outlook.com>
2024-11-24 16:18:58 -0800

    REFACTOR: Requirement schema default factory and methods

--- backend/src/schema/requirement_schema.py
+++ backend/src/schema/requirement_schema.py
@@ -1,14 +1,26 @@
-from uuid import UUID
-from pydantic import BaseModel, Field
-from src.schema.task_schema import TaskSchema
+from uuid import uuid4
+
+from sqlalchemy import UUID
+from src.models.domain.requirement import Requirement
+from src.models.domain.task import Task
+from src.schema.task_schema import BaseSchema, Field, TaskSchema
 
 
-class RequirementSchema(BaseModel):
+class RequirementSchema(BaseSchema[Requirement]):
     id: UUID = Field(
-        ..., alias="id", description="Unique identifier for the requirement"
+        default_factory=uuid4, description="Unique identifier for the requirement"
     )
     title: str = Field(description="Requirement title")
-    tasks: list[TaskSchema] = Field(
+    tasks: list[BaseSchema[Task]] = Field(
         default_factory=list,
         description="List of task to be completed in order to fulfill this requirement.",
     )
+
+    def to_model(self) -> Requirement:
+        model = self.model_dump()
+        model["tasks"] = [task.to_model() for task in self.tasks]
+        return Requirement(model)
+
+    @classmethod
+    def from_model(cls, model: Requirement) -> BaseSchema[Requirement]:
+        return cls(**model.__dict__)
