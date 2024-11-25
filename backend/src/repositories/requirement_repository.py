e8c7f9fdaac0436a029ea925105ed441c7124739
richard.nunes <richardl1010@outlook.com>
2024-11-24 16:19:25 -0800

    REFACTOR: Requirement Repo added component deco

--- backend/src/repositories/requirement_repository.py
+++ backend/src/repositories/requirement_repository.py
@@ -1,8 +1,10 @@
 from uuid import UUID
+from src.config.dependency_injection.component import component
 from src.models.domain.requirement import Requirement
 from src.utils.base_repository import BaseRepository
 
 
+@component(BaseRepository[Requirement, UUID])
 class RequirementRepository(BaseRepository[Requirement, UUID]):
 
     model = Requirement
