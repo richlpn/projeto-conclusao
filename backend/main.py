12391f7ff3865902e16373dd57bde96a68020024
richard.nunes <richardl1010@outlook.com>
2024-11-27 16:21:14 -0800

    MIGRATION: UUID on Tasks

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 # %%
 from fastapi import FastAPI
-from src.controller.schema_controller import router
+
+from src.controller.data_source_controller import router
+
 from src.controller.requirement_controller import router as req_router
 
 app = FastAPI(debug=True)
 app.include_router(router)
-# app.include_router(req_router)
+app.include_router(req_router)
