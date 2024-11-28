5be5a79009efe6ac61efdd416793b42b194c877d
richard.nunes <richardl1010@outlook.com>
2024-11-27 16:22:46 -0800

    Settings: Class

--- backend/src/config/database.py
+++ backend/src/config/database.py
@@ -1,5 +1,3 @@
-import os
-from contextlib import contextmanager
 from functools import lru_cache
 
 from sqlalchemy import create_engine
@@ -7,33 +5,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from src.config import get_settings
 
-engine = create_engine(get_settings().database_url, pool_pre_ping=True)  # type: ignore
+engine = create_engine(str(get_settings().database_url), pool_pre_ping=True)  # type: ignore
 
 Base = declarative_base()
 
 
 @lru_cache
-def create_session() -> scoped_session:
-    Session = scoped_session(
+def create_session():
+    SessionLocal = scoped_session(
         sessionmaker(autocommit=False, autoflush=False, bind=engine)
     )
-    return Session
+
+    return SessionLocal
 
 
 def create_tables():
     Base.metadata.create_all(engine)
-
-
-@contextmanager
-def get_session():
-    """Provide a transactional scope around a series of operations."""
-    session = create_session()
-    try:
-        yield session
-        session.commit()
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
-        session = None
