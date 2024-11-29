2ca23da8fda086acaeed7dfc971d2979be93dbc0
richard.nunes <richardl1010@outlook.com>
2024-11-28 17:19:20 -0800

    ADD: Tasks Controller

--- backend/src/config/app.py
+++ backend/src/config/app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from src.controller.data_source_controller import router as data_source_router
 from src.controller.requirement_controller import router as requirement_router
+from src.controller.task_controller import router as task_router
 
 app = FastAPI(debug=True)
 app.include_router(data_source_router)
 app.include_router(requirement_router)
+app.include_router(task_router)
---
+++ backend/src/controller/task_controller.py
@@ -0,0 +1,48 @@
+from uuid import UUID
+from fastapi import APIRouter, Depends
+from src.models.domain.task import Task
+from src.service.task_service import get_task_service
+from src.schema.task_schema import TaskCreateSchema, TaskSchema, TaskUpdateSchema
+from src.utils.base_service import BaseService
+
+router = APIRouter(prefix="/tasks", tags=["Tasks"])
+
+
+ServiceType = BaseService[
+    Task,
+    TaskCreateSchema,
+    TaskUpdateSchema,
+    TaskSchema,
+    UUID,
+]
+
+
+@router.get("/all")
+async def get(service: ServiceType = Depends(get_task_service)):
+    return service.get_all()
+
+
+@router.get("/")
+async def read_task(task_id: UUID, service: ServiceType = Depends(get_task_service)):
+    return service.get_by_id(task_id)
+
+
+@router.post("/")
+async def create_task(
+    task: TaskCreateSchema, service: ServiceType = Depends(get_task_service)
+):
+    return service.create(task)
+
+
+@router.put("/")
+async def update_task(
+    task_id: UUID,
+    task: TaskUpdateSchema,
+    service: ServiceType = Depends(get_task_service),
+):
+    return service.update(task_id, task)
+
+
+@router.delete("/")
+async def delete_task(task_id: UUID, service: ServiceType = Depends(get_task_service)):
+    service.delete(task_id)
