8dcece2a29c47d454e86f9dd3033f5ee9db5cd2d
richard.nunes <richardl1010@outlook.com>
2024-12-15 06:22:51 -0800

    ADD: DataSourceType Schemas

--- backend/src/schema/data_source_schema.py
+++ backend/src/schema/data_source_schema.py
@@ -1,12 +1,10 @@
-from typing import Optional
 import uuid
+from typing import Optional
 
 from pydantic import Field
-from src.models.domain.data_source.data_source_type import DataSourceType
-from src.schema.data_source_column_schema import (
-    DataSourceColumnSchema,
-)
 from src.schema.base_schema import BaseSchema
+from src.schema.data_source_column_schema import DataSourceColumnSchema
+from src.schema.data_source_type_schema import DataSourceTypeSchema
 
 
 class DataSourceCreateSchema(BaseSchema):
@@ -27,7 +25,7 @@ class DataSourceUpdateSchema(BaseSchema):
 
 class DataSourceSchema(DataSourceCreateSchema):
     id: uuid.UUID = Field(default_factory=uuid.uuid4)
-    type: DataSourceType = Field(description="File type.")  # type: ignore
+    type: DataSourceTypeSchema = Field(description="File type.")  # type: ignore
     columns: list[DataSourceColumnSchema] = Field(  # type: ignore
         default_factory=list, description="Columns used or referenced on the table."
     )
