fdafdb06cd3326adff11ed218d5ec9256fd166be
richard.nunes <richardl1010@outlook.com>
2024-12-15 06:24:01 -0800

    REFACTOR: App accept cors

--- backend/src/config/app.py
+++ backend/src/config/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.config import get_settings
 from src.controller.data_source_column_controller import (
     router as data_source_column_router,
@@ -8,7 +9,21 @@ from src.controller.requirement_controller import router as requirement_router
 from src.controller.task_controller import router as task_router
 
 settings = get_settings()
+
 app = FastAPI(debug=settings.debug)
+
+origins = [
+    "http://localhost:5173",  # frontend service URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(data_source_router)
 app.include_router(requirement_router)
 app.include_router(task_router)
