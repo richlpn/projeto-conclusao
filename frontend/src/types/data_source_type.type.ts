3226911e0b607a4b3ab6b338534f5d0b7c7ed4e1
richard.nunes <richardl1010@outlook.com>
2024-12-15 07:22:43 -0800

    REFACTOR: Added Create Schema for DataSourceType and included id on Schema

--- frontend/src/types/data_source_type.type.ts
+++ frontend/src/types/data_source_type.type.ts
@@ -1,7 +1,13 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const DataSourceTypeSchema = z.object({
-  name: z.string().min(1, 'Name is required'),
+export const DataSourceTypeCreateSchema = z.object({
+  name: z.string().min(1),
 });
 
-export type DataSourceType = z.infer<typeof DataSourceTypeSchema>
+export const DataSourceTypeSchema = z.object({
+  id: z.string().uuid(),
+  name: z.string().min(1, "Name is required"),
+});
+
+export type DataSourceType = z.infer<typeof DataSourceTypeSchema>;
+export type DataSourceTypeCreate = z.infer<typeof DataSourceTypeCreateSchema>;
