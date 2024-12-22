cd8c18e5eaa9f62778acc7a4a81801a46635921a
richard.nunes <richardl1010@outlook.com>
2024-12-22 14:53:43 -0800

    REFACTOR: Removed data source provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,12 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { DataSourceProvider } from "./context/data_sources_context.tsx";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <DataSourceProvider>
-        <App />
-      </DataSourceProvider>
+      <App />
     </QueryClientProvider>
   </StrictMode>
 );
